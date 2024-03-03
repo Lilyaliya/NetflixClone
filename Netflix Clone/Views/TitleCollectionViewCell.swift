65ac10a56a4f309f3da67a0079492ef1a2b7257d
Lilyaliya <lilyaliya@inbox.ru>
2024-03-04 01:26:11 +0300

    refactor(Models): refactor models + sdwebImage ext + new titleCollectionViewCell with poster

---
+++ Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix Clone
+//
+//  Created by User on 04.03.2024.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+}
